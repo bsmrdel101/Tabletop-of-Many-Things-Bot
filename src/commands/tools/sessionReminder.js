f295f0ca873697a2aff1f71b6c242d4febee55f7
Bennett Smrdel <fireknights318@gmail.com>
2024-04-13 16:11:05 -0500

    refactor: Can search for specific event

--- src/commands/tools/sessionReminder.js
+++ src/commands/tools/sessionReminder.js
@@ -4,16 +4,21 @@ const { getClosestEventByName, sendEventReminderMsg } = require('../../scripts/s
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('session-reminder')
-    .setDescription('Remind everyone about the session.'),
+    .setDescription('Remind everyone about the session.')
+    .addStringOption((option) => 
+        option.setName('eventname')
+          .setDescription('Name of the event')
+          .setRequired(true)),
   async execute(interaction, client) {
-    await interaction.deferReply();
-    const event = await getClosestEventByName('Evermoon');
+    const eventName = interaction.options.getString('eventname');
+    const event = await getClosestEventByName(eventName);
+    if (!event) return;
     const newMsg = await sendEventReminderMsg(client, event);
 
     if (newMsg) {
-      await interaction.editReply(newMsg);
+      await interaction.reply(newMsg);
     } else {
-      await interaction.editReply('_ _');
+      await interaction.reply('_ _');
     }
   }
 };
